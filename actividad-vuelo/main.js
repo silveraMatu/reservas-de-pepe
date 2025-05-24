34f69c0d52596268ee3207b8de188c309c1e6022
Matias Silvera <matusilvera808@gmail.com>
2025-05-24 04:47:50 -0300

    Se agrega la logica al evento submit y con esto el dev se va a dormir

--- actividad-vuelo/main.js
+++ actividad-vuelo/main.js
@@ -5,14 +5,14 @@ const obtenerDatosAvion =()=>{
     let datosObtenidos = {
         nombre: document.querySelector('#nombre').value, 
         dni: document.querySelector('#dni').value, 
-        teléfono: document.querySelector('#telefono').value, 
+        telefono: document.querySelector('#telefono').value, 
         origen: document.querySelector('#origen').value, 
         destino: document.querySelector('#destino').value, 
         fecha: document.querySelector('#fecha').value, 
         hora: document.querySelector('#hora').value, 
         clase: document.querySelector('#clase').value, 
         equipaje: document.querySelector('#equipaje').checked ? //Controla si el checkbox esta marcado
-            'Lleva equipaje adicional' : 'No lleva equipaje adicional' 
+            'Si' : 'No' 
     }
 
     return datosObtenidos;
@@ -38,7 +38,24 @@ const mostrarReservaAvion = (reserva)=>{
         <p><strong>Fecha: </strong>${reserva.fecha}</p>
         <p><strong>Hora: </strong>${reserva.hora}</p>
         <p><strong>Clase: </strong>${reserva.clase}</p>
-        <p><strong>Equipaje: </strong>${reserva.equipaje}</p>
+        <p><strong>Equipaje Adicional: </strong>${reserva.equipaje}</p>
         --------------------------------------------------------
     `;
 };
+
+//Se captura el elemento form y se le añade event listener que se ejecuta cuando
+//el usuario hace click en el botón (submit/enviar)
+document.querySelector('#formReservaAvion').addEventListener('submit', (e) =>{
+
+    e.preventDefault(); //Para que la pagina no se refresque
+
+    const reserva = obtenerDatosAvion(); //Guardamos en memoria
+
+    agregarReservaAvion(reserva); //Guardamos esos datos en un array
+
+    mostrarReservaAvion(reserva); //Mostramos en el resumen
+
+    document.querySelector('#formReservaAvion').reset(); //El formulario se resetea
+
+    console.log(reservasAvion); //Se imprime en consola las reserva
+})
